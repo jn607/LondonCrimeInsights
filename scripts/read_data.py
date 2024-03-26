842e3f75802346c3bf3299837a9cbcee8c16266f
jn607 <joshynolan02@gmail.com>
2024-03-26 17:54:48 +0000

    renamed csv files, created read_data script

---
+++ scripts/read_data.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+historical_data_raw = pd.read_csv('/home/jn607/Projects/LondonCrimeInsights/data/raw_data/APR2010-FEB2022.csv')
+recent_data_raw = pd.read_csv('/home/jn607/Projects/LondonCrimeInsights/data/raw_data/FEB2022-FEB2024.csv')
