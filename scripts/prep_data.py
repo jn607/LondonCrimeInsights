cdecd5f4e245eb132deaf768871df6078b998789
Josh Nolan <joshynolan02@gmail.com>
2024-08-26 20:43:06 +0100

    Updated prep_data file

--- scripts/prep_data.py
+++ scripts/prep_data.py
@@ -18,64 +18,6 @@ def read_and_inspect(file_path, columns_to_inspect):
         print(df[column].drop_duplicates())
     return df
 
-def analyse_missing_values(df):
-    """
-    Analyses missing values from df.
-
-    Parameters:
-    df (DataFrame): The pandas DataFrame with data.
-
-    Returns:
-    Count of rows and columns that have at least one null value and the related percentage.
-    """
-    missing_rows = df.isnull().any(axis=1).sum()
-    missing_columns = df.isnull().any(axis=0).sum()
-    total_rows, total_columns = df.shape
-    print(f"Missing rows: {missing_rows} out of {total_rows} ({missing_rows/total_rows*100:.2f}%)")
-    print(f"Missing columns: {missing_columns} out of {total_columns} ({missing_columns/total_columns*100:.2f}%)")
-
-def aggregate_years(df, year_columns, non_time_columns):
-    """
-    Aggregates monthly data into yearly data.
-
-    Parameters:
-    df (DataFrame): The pandas DataFrame with monthly data.
-    year_columns (list): List of years to aggregate data for.
-
-    Returns:
-    DataFrame: A new DataFrame with data aggregated by year.
-    """
-    df_yearly = pd.DataFrame()
-    for year in year_columns:
-        cols_for_year = [col for col in df.columns if col.startswith(year)]
-        df_yearly[year] = df[cols_for_year].sum(axis=1)
-    df_yearly[non_time_columns] = df[non_time_columns]
-    return df_yearly[non_time_columns + sorted(year_columns)]
-
-def detect_outliers(df, numeric_columns):
-    """
-    Detects outliers in the numeric columns of a DataFrame using the Interquartile Range (IQR) method.
-
-    Parameters:
-    df (DataFrame): The pandas DataFrame to analyze for outliers.
-    numeric_columns (list): List of column names in df that are numeric.
-
-    Returns:
-    tuple: 
-        - DataFrame containing only the rows identified as outliers in numeric columns.
-        - Series indicating the count of outliers in each numeric column.
-    """
-    Q1 = df[numeric_columns].quantile(0.25)
-    Q3 = df[numeric_columns].quantile(0.75)
-    IQR = Q3 - Q1
-
-    outlier_condition = ((df[numeric_columns] < (Q1 - 1.5 * IQR)) | (df[numeric_columns] > (Q3 + 1.5 * IQR)))
-    outliers = outlier_condition.any(axis=1)
-    outliers_df = df[outliers]
-    outliers_count = outlier_condition.sum()
-
-    return outliers_df, outliers_count
-
 # Set display options
 pd.set_option('display.max_columns', None)
 
@@ -93,30 +35,3 @@ df_crime_monthly = pd.merge(df_historical_data_raw, df_recent_data_raw, how='out
 df_crime_monthly.to_csv('/Users/joshnolan/programmingProjects/CrimeInsights/LondonCrimeInsights/data/bronze_data/bronze_crime_monthly.csv', index=False)
 
 
-# Inspect the merged data
-print("Shape:", df_crime_monthly.shape)
-print("Info:")
-print(df_crime_monthly.info())
-
-# Missing values analysis
-print("Monthly df:")
-analyse_missing_values(df_crime_monthly)
-
-# Aggregate data into yearly
-non_time_columns = ['MajorText', 'MinorText', 'LookUp_BoroughName']
-years = sorted(set(col[:4] for col in df_crime_monthly.columns if col.isdigit()))
-df_crime_yearly = aggregate_years(df_crime_monthly, years, non_time_columns)
-
-# Display the first few rows of the yearly data
-print(df_crime_yearly.head())
-
-# Missing values analysis for yearly data
-print("Yearly df:")
-analyse_missing_values(df_crime_yearly)
-
-# outlier analysis
-numeric_cols = df_crime_monthly.select_dtypes(include=['number']).columns.tolist()
-outliers_df, outliers_count = detect_outliers(df_crime_monthly, numeric_cols)
-
-print("Outliers detected in each numeric column:\n", outliers_count)
-print("\nRows with outliers in numeric columns:\n", outliers_df)
